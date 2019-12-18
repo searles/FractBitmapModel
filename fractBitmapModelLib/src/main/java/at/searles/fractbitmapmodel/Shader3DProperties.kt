d44d427fd51c9edca417405b65ce5c3bd6741466
searles <karl.gmeiner@gmail.com>
2019-12-18 02:01:59 +0100

    Fix pixGap in RS

--- fractBitmapModelLib/src/main/java/at/searles/fractbitmapmodel/Shader3DProperties.kt
+++ fractBitmapModelLib/src/main/java/at/searles/fractbitmapmodel/Shader3DProperties.kt
@@ -6,11 +6,11 @@ import kotlin.math.sin
 
 class Shader3DProperties {
     var useLightEffect: Boolean = true
-    var lightVector: Float3 = Float3(-1f/3f, -2f/3f, 2f/3f)
+    var lightVector: Float3 = Float3(-2f/3f, -2f/3f, 1f/3f)
     var ambientReflection = 0f
     var diffuseReflection = 1f
     var specularReflection = 1f
-    var shininess: Int = 4 // FIXME this ain't workin'
+    var shininess: Int = 4
 
     fun setLightVector(polarAngle: Float, azimuthAngle: Float) {
         lightVector = Float3(
